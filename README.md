bad3cbe553cd6d280ae4a3a764841f7ff2791367
Summiyah Siddeeq <summiyah@gmail.com>
2017-12-19 01:23:50 -0500

    added last part of assignment. Where random.txt is read with spotify info

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # liri-node-app
+
+Created node app that logs Tweets, IMDB movie info & Spotify song info. 
+
+All of my keys are on .gitignore so they cannot be seen. 
--- liri.js
+++ liri.js
@@ -1,30 +1,26 @@
-// write the code you need to grab the data from keys.js. Then store the keys in a variable.
-// make liri.js have one of the following commands my-tweets - spotify-this-song - movie-this -
-//do-what-it-says
-// 3d226de009fd475d869c32dec5838b35
-// f43c7773aa8a4d419a4108b8decc7381 secret spotify key
-
-//node liri.js movie-this '<Creed>'
-
-//var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
 
 var myTweets = process.argv[2] === "my-tweets";
 var mySpotify = process.argv[2] === "spotify-this-song";
 var myMovies = process.argv[2] === "movie-this";
+var doWhatItSays = process.argv[2] === "do-what-it-says";
 
+// all requires here
 var Twitter = require('twitter');
 var request = require('request');
 var Spotify = require('node-spotify-api');
+var fs = require('fs');
 var keys = require("./keys.js");
 
 var twitterClient = new Twitter(keys);
 
+// used Erykah Badu's twitter - she follows me since 2010!
+// my keys are .gitignored
 if (myTweets) {
     var twitterParams = {screen_name: 'fatbellybella', count: 20};
     twitterClient.get('statuses/user_timeline', twitterParams, function (error, tweets, response) {
         if (!error) {
-            for (var i = 0; i < tweets.length ; i++) {
+            for (var i = 0; i < tweets.length; i++) {
                 var tweet = tweets[i];
                 console.log(tweet.text);
                 console.log(tweet.created_at);
@@ -37,20 +33,24 @@ var spotifyClient = new Spotify({
     id: "3d226de009fd475d869c32dec5838b35",
     secret: "f43c7773aa8a4d419a4108b8decc7381",
 });
-
+// #4 - add funtion for getting spotify to work on random.txt - Final step
 if (mySpotify) {
-    var spotifyParams = {type: 'track', query: process.argv[3] };
-    spotifyClient.search( spotifyParams, function(err, data) {
+    spotifyThisSong(process.argv[3]);
+}
+
+function spotifyThisSong(mySong) {
+    var spotifyParams = {type: 'track', query: mySong};
+    spotifyClient.search(spotifyParams, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        for (var i =0; i < data.tracks.items.length ; i++) {
+        for (var i = 0; i < data.tracks.items.length; i++) {
             var item = data.tracks.items[i];
             console.log("Artist: ", item.artists[0].name);
             console.log("Song: ", item.name);
             console.log("Preview: ", item.preview_url);
             console.log("Album: ", item.album.name);
-            // add extra line for space
+            // add extra line break for space between songs
             console.log("===========================")
         }
     });
@@ -64,11 +64,12 @@ if (myMovies) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+        // was a string not object like I originally though. 
         var body = JSON.parse(bodyString);
         console.log("Movie: ", body.Title);
         console.log("Release Date: ", body.Year);
         console.log("Rating: ", body.Rated);
-        for (var i =0; i < body.Ratings.length ; i++) {
+        for (var i = 0; i < body.Ratings.length; i++) {
             var rating = body.Ratings[i];
             if (rating.Source === "Internet Movie Database") {
                 console.log("IMDB Rating: ", rating.Value);
@@ -83,26 +84,18 @@ if (myMovies) {
         console.log("Actors: ", body.Actors);
     });
 
-
 }
 
+if (doWhatItSays) {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        spotifyThisSong(data);
+    });
+}
 
-
-
-// TODO: check if myMovies
-// TODO: use process.argv[3] to query omdb
-// if (operand === "add") {
-//     outputNum = parseFloat(num1) + parseFloat(num2);
-// }
-//
-// else if (operand === "subtract") {
-//     outputNum = parseFloat(num1) - parseFloat(num2);
-// }
-
-
-// var Spotify = require('spotify');
-//
-// var keys = require("./keys.js");
 
 
 
